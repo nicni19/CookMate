4a05f16e948c26df0e0a190e56e6bc6721158583
AndreasEdal <anepe19@student.sdu.dk>
2021-10-29 14:35:44 +0200

    Made the followers of the Cookbook ready to do lazy loading

--- CookMate/src/shared/view-models/Cookbook.ts
+++ CookMate/src/shared/view-models/Cookbook.ts
@@ -3,13 +3,19 @@ import {UserSimple} from "./UserSimple";
 import {RecipeSimple} from "./RecipeSimple";
 
 export class Cookbook extends CookbookSimple {
-    followers : UserSimple[];
+
+    get followers(): UserSimple[] {
+        // this._followers =    // TODO: Get followers from model as these only need to be loaded when explicitly asked for
+        return this._followers;
+    }
+    private _followers : UserSimple[];
+
     recipes : RecipeSimple[];
 
 
     constructor(id: string, name: string, owner: UserSimple, followers: UserSimple[], recipes: RecipeSimple[]) {
         super(id, name, owner);
-        this.followers = followers;
+        this._followers = followers;
         this.recipes = recipes;
     }
 }
