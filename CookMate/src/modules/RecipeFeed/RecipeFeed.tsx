ba7276b0be307ebd8826057b13b30a905b01d338
Bolding22 <w.bolding@outlook.com>
2021-10-29 11:56:55 +0200

    Further progress on styling the feed.

--- CookMate/src/modules/RecipeFeed/RecipeFeed.tsx
+++ CookMate/src/modules/RecipeFeed/RecipeFeed.tsx
@@ -2,23 +2,61 @@ import React, {Component} from 'react';
 import { SafeAreaView, StyleSheet, View, FlatList, Text } from 'react-native';
 import Card from './Card';
 
-const DATA = ['TestCard', 'TestCard2']
+const DATA = ['TestCard', 'TestCard2', '3', '4', '5', '6']
 const cardComponents = DATA.map(type => <Card></Card>)
 
 class RecipeFeed extends Component {
     styles = StyleSheet.create({
-        container: {
-            margin: 50,
+        horizontalCard: {
+            height: 170,
+            width: 118,
+            margin: 5,
+            borderWidth: 0,
+            borderRadius: 15,
+            padding: 3,
+            flex: 1,
+            backgroundColor: "#af3c3c",
+            alignItems: "center",
+        },
+        verticalCard: {
+            height: 170,
+            width: 118,
+            margin: 5,
+            borderWidth: 0,
+            borderRadius: 15,
+            padding: 3,
+            backgroundColor: "#167542",
+            alignItems: "center",
+        },
+        horizontalContainer: {
+            width: "100%",
+            margin: 0,
+            borderWidth: 0,
+            flex: 1,
+        },
+        verticalContainer: {
+            width: "100%",
+            margin: 0,
+            borderWidth: 0,
+            flex: 3,
+            alignSelf: "center",
         }
     })
     render() {
         return (
             <View>
-                <SafeAreaView style={this.styles.container}>
+                <SafeAreaView style={this.styles.horizontalContainer}>
                 <FlatList
                     data={cardComponents}
-                    renderItem={item => <Card cardStyle={this.styles.container}/>}
+                    renderItem={item => <Card cardStyle={this.styles.horizontalCard}/>}
                     horizontal={true}
+                    />
+                </SafeAreaView>
+                <SafeAreaView style={this.styles.verticalContainer}>
+                    <FlatList
+                        data={cardComponents}
+                        renderItem={item => <Card cardStyle={this.styles.verticalCard}/>}
+                        numColumns={2}
                     />
                 </SafeAreaView>
             </View>
