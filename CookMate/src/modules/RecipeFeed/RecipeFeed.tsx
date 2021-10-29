86ae3cd3c21c35d0c273adbc2bec2079bf0b4750
beand19 <beand19@student.sdu.dk>
2021-10-29 10:08:29 +0200

    Add style for Card component

--- CookMate/src/modules/RecipeFeed/RecipeFeed.tsx
+++ CookMate/src/modules/RecipeFeed/RecipeFeed.tsx
@@ -1,10 +1,29 @@
 import React, {Component} from 'react';
-import { View, FlatList, Text } from 'react-native';
+import { SafeAreaView, StyleSheet, View, FlatList, Text } from 'react-native';
+import Card from './Card';
+
+const DATA = ['TestCard', 'TestCard2']
+const cardComponents = DATA.map(type => <Card></Card>)
 
 class RecipeFeed extends Component {
+    styles = StyleSheet.create({
+        container: {
+            margin: 50,
+        }
+    })
     render() {
         return (
-            <View></View>
+            <View>
+                <SafeAreaView style={this.styles.container}>
+                <FlatList
+                    data={cardComponents}
+                    renderItem={item => <Card cardStyle={this.styles.container}/>}
+                    horizontal={true}
+                    />
+                </SafeAreaView>
+            </View>
         )
     }
 }
+
+export default RecipeFeed;
