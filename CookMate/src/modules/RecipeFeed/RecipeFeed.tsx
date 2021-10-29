9cac4b2f6c35bb890d09733564522d1bc99c328a
beand19 <beand19@student.sdu.dk>
2021-10-29 08:59:01 +0200

    Add Card.tsx file for recipefeed

---
+++ CookMate/src/modules/RecipeFeed/RecipeFeed.tsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import { View, FlatList, Text } from 'react-native';
+
+class RecipeFeed extends Component {
+    render() {
+        return (
+            <View></View>
+        )
+    }
+}
