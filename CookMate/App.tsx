d6401ae166da8bfec8a21ed1c7e5efc28e7bb4d0
Wasim Choukair <wasimchoukair@gmail.com>
2021-11-04 22:07:57 +0100

    Adds CreateRecipeForm as a shared screen component

--- CookMate/App.tsx
+++ CookMate/App.tsx
@@ -1,15 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, SafeAreaView } from "react-native";
+import { StyleSheet, SafeAreaView } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import CreateRecipeForm from "./src/modules/create-recipe/CreateRecipeForm";
+import CreateRecipeScreen from "./src/shared/components/screens/CreateRecipeScreen";
 
 export default function App() {
     return (
         <SafeAreaView style={styles.container}>
             <StatusBar style="auto" />
-            <CreateRecipeForm />
+            <CreateRecipeScreen />
         </SafeAreaView>
     );
 }
--- CookMate/src/modules/create-recipe/CreateRecipeStyles/CreateRecipeStyles.ts
+++ CookMate/src/modules/create-recipe/CreateRecipeStyles/CreateRecipeStyles.ts
@@ -1,21 +1,8 @@
 import { StyleSheet } from "react-native";
 
-export const styles = () =>
-    StyleSheet.create({
-        textfield: {
-            flex: 1,
-            backgroundColor: "#fff",
-            alignItems: "center",
-            justifyContent: "center"
-        },
-        image: {},
-        button: {}
-    });
-
-export const arrowBtnStyles = (arrowColor: string) =>
-    StyleSheet.create({
-        arrowBtn: {
-            backgroundColor: "transparent",
-            color: arrowColor
-        }
-    });
+export const styles = StyleSheet.create({
+    root: {},
+    textfield: {},
+    button: {},
+    arrowBtn: {}
+});
