09cd234f22a22414c6fd5fe8c70a186ffc9e9d1c
Jeppe Stenstrup Lauridsen <jelau19@student.sdu.dk>
2021-11-07 11:39:09 +0100

    Added comments

--- CookMate/src/shared/services/QueryService.ts
+++ CookMate/src/shared/services/QueryService.ts
@@ -11,7 +11,13 @@ export default class QueryService {
     public static users : IUserQueryService = {
         getUser: function (userId: string): User {
             const data = require('../../../assets/data/user.db.json');
+
+            // Deconstructing data from user.db.json into variables
+            // If data.find(...) returns null or fails, an empty object is returned and the variables are set to null
             let { id, firstname, lastname, cookbookId } = data.find((item: any) => item.id == userId) || {}
+            
+            // If "id" is not null, return a new User
+            // Else return null
             return id && new User(
                 id,
                 firstname,
@@ -21,8 +27,13 @@ export default class QueryService {
         },
         getUserSimple: function (userId: string): UserSimple {
             const data = require('../../../assets/data/user.db.json');
+
+            // Deconstructing data from user.db.json into variables
+            // If data.find(...) returns null or fails, an empty object is returned and the variables are set to null
             let { id, firstname, lastname, profilepicture } = data.find((item: any) => item.id == userId) || {}
 
+            // If "id" is not null, return a new UserSimple
+            // Else return null
             return id && new UserSimple(
                 id,
                 firstname,
@@ -31,6 +42,11 @@ export default class QueryService {
         },
         getFollowersOfCookbook: function (cookbookId: string): CookbookSimple[] {
             const data = require('../../../assets/data/follower_relation.db.json');
+
+            // Filter data to get only the items we need
+            // Map the data from an object to an array of new CookbookSimple
+            // If data.filter(...).map(...) returns null or fails, null is returned
+            // "?." means that, if .filter(...) fails or returns null, .map doesn't run and saves memory and processing time, and falls back to "|| null"
             return data.filter((item: any) => item.cookbookId == cookbookId)
                        ?.map((item: any) => (
                            new CookbookSimple(
