7c459746b2672444f887630846830e9f5dc18d4c
Wasim Choukair <wasimchoukair@gmail.com>
2021-11-12 15:47:26 +0100

    Fixes content going out of screen view

--- CookMate/src/modules/RecipeFeed/RecipeFeed.tsx
+++ CookMate/src/modules/RecipeFeed/RecipeFeed.tsx
@@ -1,7 +1,7 @@
-import React, {Component} from 'react';
-import { SafeAreaView, StyleSheet, View, FlatList, Text } from 'react-native';
-import Card from './Card';
-import data from './recipes.json';
+import React, { Component } from "react";
+import { SafeAreaView, StyleSheet, View, FlatList, Text } from "react-native";
+import Card from "./Card";
+import data from "./recipes.json";
 
 class RecipeFeed extends Component {
     styles = StyleSheet.create({
@@ -16,7 +16,7 @@ class RecipeFeed extends Component {
             padding: 0,
             flex: 1,
             backgroundColor: "#af3c3c",
-            alignItems: "center",
+            alignItems: "center"
         },
         verticalCard: {
             width: "43%",
@@ -28,7 +28,7 @@ class RecipeFeed extends Component {
             borderRadius: 10,
             padding: 0,
             backgroundColor: "#167542",
-            alignItems: "flex-start",
+            alignItems: "flex-start"
         },
         horizontalContainer: {
             width: 300,
@@ -37,7 +37,7 @@ class RecipeFeed extends Component {
             padding: 0,
             marginLeft: "4%",
             borderWidth: 0,
-            flex: 2,
+            flex: 2
         },
         verticalContainer: {
             width: "100%",
@@ -47,62 +47,81 @@ class RecipeFeed extends Component {
             marginLeft: "4%",
             marginRight: "0%",
             borderWidth: 0,
-            flex: 6 ,
+            flex: 6
             //alignSelf: "center",
         },
         text: {
-            fontFamily: "Roboto",
+            fontFamily: "Roboto"
         },
         titleText: {
             fontFamily: "Roboto",
             fontSize: 20,
-            fontWeight: "bold",
+            fontWeight: "bold"
         },
         lineView: {
-            borderBottomColor: 'black',
+            borderBottomColor: "black",
             width: 328,
             borderBottomWidth: 1,
             marginTop: 10,
             marginBottom: 4,
             marginLeft: "4%",
-            marginRight: "4%",
+            marginRight: "4%"
         },
         image: {
             width: "100%",
             height: 100,
             borderTopRightRadius: 10,
-            borderTopLeftRadius: 10,
+            borderTopLeftRadius: 10
         },
         profileImage: {
             width: "70%",
             marginTop: "13%",
             marginBottom: "8%",
             height: 80,
-            borderRadius: 50,
-        },
-    })
+            borderRadius: 50
+        }
+    });
     render() {
         return (
-            <View>
+            <View style={{ flex: 1 }}>
                 <SafeAreaView style={this.styles.horizontalContainer}>
                     <Text style={this.styles.titleText}>Cookbooks</Text>
                     <FlatList
                         data={data.cookbooks}
-                        renderItem={({item}) => <Card cardStyle={this.styles.horizontalCard} imageStyle={this.styles.profileImage} title={item.title} author={item.author} imageUrl={item.imageUrl}/>}
+                        renderItem={({ item }) => (
+                            <Card
+                                cardStyle={this.styles.horizontalCard}
+                                imageStyle={this.styles.profileImage}
+                                title={item.title}
+                                author={item.author}
+                                imageUrl={item.imageUrl}
+                            />
+                        )}
                         horizontal={true}
                     />
                 </SafeAreaView>
                 <View style={this.styles.lineView}></View>
                 <SafeAreaView style={this.styles.verticalContainer}>
-                    <Text style={this.styles.text}>News from cookbooks you follow</Text>
+                    <Text style={this.styles.text}>
+                        News from cookbooks you follow
+                    </Text>
                     <FlatList
                         data={data.recipes}
-                        renderItem={({item}) => <Card cardStyle={this.styles.verticalCard} imageStyle={this.styles.image} title={item.title} duration={item.duration} persons={item.persons} imageUrl={item.imageUrl}/>}
+                        renderItem={({ item }) => (
+                            <Card
+                                cardStyle={this.styles.verticalCard}
+                                imageStyle={this.styles.image}
+                                title={item.title}
+                                duration={item.duration}
+                                persons={item.persons}
+                                imageUrl={item.imageUrl}
+                            />
+                        )}
                         numColumns={2}
                     />
                 </SafeAreaView>
             </View>
-        )
+        );
     }
 }
 
