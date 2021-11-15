bee6d1fd81364c773f093ce9fb210a858816cb44
AndreasEdal <anepe19@student.sdu.dk>
2021-11-15 20:59:55 +0100

    Fixed Providers to what it's supposed to be

--- CookMate/src/shared/components/Providers.tsx
+++ CookMate/src/shared/components/Providers.tsx
@@ -1,15 +1,13 @@
 import React from "react";
 import { Routes } from "./Routes";
 import { AuthProvider } from "./auth/AuthProvider";
-import {UserProfile} from "../../modules/user-profile-screen/UserProfile";
 
 interface ProvidersProps {}
 
 export const Providers: React.FC<ProvidersProps> = () => {
     return (
-        <UserProfile userId={"1"}/>
-        /*<AuthProvider>
+        <AuthProvider>
             <Routes />
-        </AuthProvider>*/
+        </AuthProvider>
     );
 };
